d39287493730aaa0305f22891a1a6e2884445e91
Mrbrokenlegs <randall@lunchbox.io>
2024-06-27 01:22:02 -0400

    add moving background

--- main.lua
+++ main.lua
@@ -7,7 +7,15 @@ VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local Background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
 local Ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -30,11 +38,18 @@ function love.keypressed(key)
   end
 end
 
+function love.update(dt)
+  backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+  print(backgroundScroll)
+
+  groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
   Push:start()
-  love.graphics.draw(Background, 0, 0)
+  love.graphics.draw(Background, -backgroundScroll, 0)
   -- 16 is the pixel height of the ground image
   -- We need to subtract it from the virtual height to see the ground on screen
-  love.graphics.draw(Ground, 0, VIRTUAL_HEIGHT - 16)
+  love.graphics.draw(Ground, -groundScroll, VIRTUAL_HEIGHT - 16)
   Push:finish()
 end
